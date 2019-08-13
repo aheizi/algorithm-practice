9256217d8446365c5463ef624f766e343fcc0964
yangquan1 <yangquan1@staff.weibo.com>
2019-08-13 22:17:21 +0800

    加题目

---
+++ src/main/java/com/aheizi/linked_list/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,30 @@
+package com.aheizi.linked_list;
+
+import com.aheizi.linked_list.data_structure.ListNode;
+
+/**
+ * Created by yangquan1 on 2019-08-13.
+ *
+ * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ *
+ * 示例 1:
+ *
+ * 输入: 1->1->2
+ * 输出: 1->2
+ * 示例 2:
+ *
+ * 输入: 1->1->2->3->3
+ * 输出: 1->2->3
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class RemoveDuplicatesFromSortedList {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        return null;
+    }
+
+}
