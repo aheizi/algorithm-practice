73b4562181ba5d6c94f410372424c3ed8eb8836d
yangquan1 <yangquan1@staff.weibo.com>
2019-08-13 22:49:21 +0800

    Remove Duplicates From Sorted List

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 2. [Swap Nodes In Pairs](src/main/java/com/aheizi/linked_list/SwapNodesInPairs.java)
 3. [Linked List Cycle](src/main/java/com/aheizi/linked_list/LinkedListCycle.java)
 4. [Merge Two Sorted Lists](src/main/java/com/aheizi/linked_list/MergeTwoSortedLists.java)
+5. [Remove Duplicates From Sorted List](src/main/java/com/aheizi/linked_list/RemoveDuplicatesFromSortedList.java)
--- src/main/java/com/aheizi/linked_list/RemoveDuplicatesFromSortedList.java
+++ src/main/java/com/aheizi/linked_list/RemoveDuplicatesFromSortedList.java
@@ -23,8 +23,52 @@ import com.aheizi.linked_list.data_structure.ListNode;
  */
 public class RemoveDuplicatesFromSortedList {
 
-    public ListNode deleteDuplicates(ListNode head) {
-        return null;
+    /**
+     * 迭代
+     *
+     * 遇到相同的元素，直接跳过
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode pre = new ListNode(-1);
+        ListNode result = pre;
+        while (head.next != null) {
+            if (head.val == head.next.val) {
+                pre.next = head.next;
+            } else {
+                pre.next = head;
+                pre = head;
+            }
+            head = head.next;
+        }
+        return result.next;
     }
 
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(1);
+        ListNode l3 = new ListNode(2);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(4);
+
+        // 1->2->4
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+        l3.next = null;
+
+        ListNode result = deleteDuplicates(l1);
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+
+    }
 }
