10c161e9b5c8c1fb0db792ba4f666fc9a77b79f9
yangquan1 <yangquan1@staff.weibo.com>
2019-08-14 21:44:58 +0800

    找出环的起点

--- README.md
+++ README.md
@@ -6,5 +6,6 @@
 1. [Reverse Linked List](src/main/java/com/aheizi/linked_list/ReverseLinkedList.java)
 2. [Swap Nodes In Pairs](src/main/java/com/aheizi/linked_list/SwapNodesInPairs.java)
 3. [Linked List Cycle](src/main/java/com/aheizi/linked_list/LinkedListCycle.java)
-4. [Merge Two Sorted Lists](src/main/java/com/aheizi/linked_list/MergeTwoSortedLists.java)
-5. [Remove Duplicates From Sorted List](src/main/java/com/aheizi/linked_list/RemoveDuplicatesFromSortedList.java)
+4. [Linked List Cycle2](src/main/java/com/aheizi/linked_list/LinkedListCycle2.java)
+5. [Merge Two Sorted Lists](src/main/java/com/aheizi/linked_list/MergeTwoSortedLists.java)
+6. [Remove Duplicates From Sorted List](src/main/java/com/aheizi/linked_list/RemoveDuplicatesFromSortedList.java)
--- src/main/java/com/aheizi/linked_list/LinkedListCycle2.java
+++ src/main/java/com/aheizi/linked_list/LinkedListCycle2.java
@@ -2,6 +2,9 @@ package com.aheizi.linked_list;
 
 import com.aheizi.linked_list.data_structure.ListNode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by yangquan1 on 2019-08-13.
  *
@@ -25,8 +28,66 @@ import com.aheizi.linked_list.data_structure.ListNode;
  */
 public class LinkedListCycle2 {
 
-    public ListNode detectCycle(ListNode head) {
+    /**
+     * 使用set保存迭代过的节点
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode detectCycle1(ListNode head) {
+        Set<ListNode> listNodes = new HashSet<ListNode>();
+        ListNode node = head;
+        while (node != null) {
+            if (listNodes.contains(node)) {
+                return node;
+            }
+            listNodes.add(node);
+            node = node.next;
+        }
         return null;
     }
 
+    /**
+     * Floyd 算法
+     *
+     * 2(F+a) = F+a+b+a
+     * 2F+2a = F+2a+b
+     * F = b
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode detectCycle2(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        // 第一步，快慢指针找出碰撞点
+        ListNode crash = getCrash(head);
+        if (crash == null) {
+            return null;
+        }
+
+        // 第二步，两个指针一步一步走，相遇即是环的起点
+        ListNode l1 = head;
+        ListNode l2 = crash;
+        while (l1 != l2) {
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        return l1;
+    }
+
+    public static ListNode getCrash(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null ) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) {
+                return fast;
+            }
+        }
+        return null;
+    }
 }
